b9bbee2d063e002e08905ca446b3604ce1594163
ajiw <ajistwn301@gmail.com>
2024-05-21 10:34:48 +0700

    sshskks

--- script.js
+++ script.js
@@ -5,139 +5,180 @@ const stopButton = document.getElementById("stop");
 const gameContainer = document.querySelector(".game-container");
 const result = document.getElementById("result");
 const controls = document.querySelector(".controls-container");
+const wrapper = document.getElementsByClassName("wrapper")[0];
 let cards;
-let interval;
 let firstCard = false;
 let secondCard = false;
 
+//mengambil inputan dari user
+const peluang = document.getElementById("kesempatan");
+const level = document.getElementById("level");
+
 const items = [
   {
-    name:'anjing',
-    image:'src/anjing.png'
+    name: "anjing",
+    image: "src/anjing.png",
   },
   {
-    name:'ayam',
-    image:'src/ayam.png'
+    name: "ayam",
+    image: "src/ayam.png",
   },
   {
-    name:'banteng',
-    image:'src/banteng.png'
+    name: "banteng",
+    image: "src/banteng.png",
   },
   {
-    name:'bebek',
-    image:'src/bebek.png'
+    name: "bebek",
+    image: "src/bebek.png",
   },
-  {
-    name:'domba',
-    image:'src/domba.png'
-  },
-  {
-    name:'embe',
-    image:'src/embe.png'
-  },
-  {
-    name:'kambing',
-    image:'src/kambing.png'
-  },
-  {
-    name:'kucing',
-    image:'src/kucing.png'
-  },
-  {
-    name:'kuda',
-    image:'src/kuda.png'
-  },
-  {
-    name:'merak',
-    image:'src/merak.png'
-  },
-  {
-    name:'puyuh',
-    image:'src/puyuh.png'
-  },
-  {
-    name:'sapi',
-    image:'src/sapi.png'
-  },
-  {
-    name:'ular',
-    image:'src/ular.png'
-  },
-  {
-    name:'penyu',
-    image:'src/penyu.png'
-  },
-  {
-    name:'monyet',
-    image:'src/monyet.png'
-  },
-  {
-    name:'pinguin',
-    image:'src/pinguin.png'
-  },
-  {
-    name:'scorpions',
-    image:'src/scorpions.png'
-  },
-  {
-    name:'lebah',
-    image:'src/lebah.png'
-  },
-  {
-    name:'nyamuk',
-    image:'src/nyamuk.png'
-  },
-  {
-    name:'loverbird',
-    image:'src/loverbird.png'
-  },
-  {
-    name:'tupai',
-    image:'src/tupai.png'
-  },
-  {
-    name:'koala',
-    image:'src/koala.png'
-  },
-  {
-    name:'katak',
-    image:'src/katak.png'
-  },
-  {
-    name:'elang',
-    image:'src/elang.png'
-  },
-  {
-    name:'singa',
-    image:'src/singa.png'
-  },
+  // {
+  //   name: "domba",
+  //   image: "src/domba.png",
+  // },
+  // {
+  //   name: "embe",
+  //   image: "src/embe.png",
+  // },
+  // {
+  //   name: "kambing",
+  //   image: "src/kambing.png",
+  // },
+  // {
+  //   name: "kucing",
+  //   image: "src/kucing.png",
+  // },
+  // {
+  //   name: "kuda",
+  //   image: "src/kuda.png",
+  // },
+  // {
+  //   name: "merak",
+  //   image: "src/merak.png",
+  // },
+  // {
+  //   name: "puyuh",
+  //   image: "src/puyuh.png",
+  // },
+  // {
+  //   name: "sapi",
+  //   image: "src/sapi.png",
+  // },
+  // {
+  //   name: "ular",
+  //   image: "src/ular.png",
+  // },
+  // {
+  //   name: "penyu",
+  //   image: "src/penyu.png",
+  // },
+  // {
+  //   name: "monyet",
+  //   image: "src/monyet.png",
+  // },
+  // {
+  //   name: "pinguin",
+  //   image: "src/pinguin.png",
+  // },
+  // {
+  //   name: "scorpions",
+  //   image: "src/scorpions.png",
+  // },
+  // {
+  //   name: "lebah",
+  //   image: "src/lebah.png",
+  // },
+  // {
+  //   name: "nyamuk",
+  //   image: "src/nyamuk.png",
+  // },
+  // {
+  //   name: "loverbird",
+  //   image: "src/loverbird.png",
+  // },
+  // {
+  //   name: "tupai",
+  //   image: "src/tupai.png",
+  // },
+  // {
+  //   name: "koala",
+  //   image: "src/koala.png",
+  // },
+  // {
+  //   name: "katak",
+  //   image: "src/katak.png",
+  // },
+  // {
+  //   name: "elang",
+  //   image: "src/elang.png",
+  // },
+  // {
+  //   name: "singa",
+  //   image: "src/singa.png",
+  // },
 ];
 
-//Initial Time
-let seconds = 0, minutes = 0;
 
-//Initial moves and win count
-let movesCount = 0, winCount = 0;
+//Initial win count
+let winCount = 0;
+let movesCount = 0;
 
 //For timer
+let seconds = 0, minutes = 1;
+let interval;
+
 const timeGenerator = () => {
-  seconds += 1;
-  if (seconds >= 60) {
-    minutes += 1;
-    seconds = 0;
+  if (seconds === 0) {
+    if (minutes > 0) {
+      minutes -= 1;
+      seconds = 10;
+    }
+  } else {
+    seconds -= 1;
   }
+
   let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
   let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
-  timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+  timeValue.innerHTML = `<span>Time:</span> ${minutesValue}:${secondsValue}`;
+
+  if (minutes === 0 && seconds === 0) {
+    clearInterval(interval); //Waktu akan berhenti apabila menit dan detik telah bernilai 0
+  }
 };
+
+// Start game
+function gameStart() {
+  sound2.play();
+  movesCount = peluang.value;
+  seconds = 0;
+  minutes = 1;
+  controls.classList.add("hide");
+  wrapper.classList.remove("hide");
+  stopButton.classList.remove("hide");
+  startButton.classList.add("hide");
+  // Start timer
+  interval = setInterval(timeGenerator, 1000);
+  // Initial moves
+  moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+  initializer();
+}
+// startButton.addEventListener("click", () => {
+//   if (!peluang || !level) {
+//     Swal.fire({
+//       title: "Peringatan",
+//       text: "Ada inputan yang masih kosong, harap di cek kembali sebelum bermain",
+//       icon: "info",
+//     });
+//   } else {
+//   }
+// });
+
 
 const movesCounter = () => {
-  movesCount += 1;
-  moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+  movesCount -= 1;
+  moves.innerHTML = `<span>Peluang :</span>${movesCount}`;
 };
 
-const generateRandom = (size = 6) => {
-
+const generateRandom = (size = 2) => {
   let tempArray = [...items];
   let cardValues = [];
   size = (size * size) / 2;
@@ -149,14 +190,13 @@ const generateRandom = (size = 6) => {
   return cardValues;
 };
 
-
 //add sound
-const sound1 = document.getElementById('sound1');
-const sound2 = document.getElementById('sound2');
-const sound3 = document.getElementById('sound3');
-const sound4 = document.getElementById('sound4');
+const sound1 = document.getElementById("sound1");
+const sound2 = document.getElementById("sound2");
+const sound3 = document.getElementById("sound3");
+const sound4 = document.getElementById("sound4");
 
-const matrixGenerator = (cardValues, size = 6) => {
+const matrixGenerator = (cardValues, size = 2) => {
   gameContainer.innerHTML = "";
   cardValues = [...cardValues, ...cardValues];
   cardValues.sort(() => Math.random() - 0.5);
@@ -173,75 +213,70 @@ const matrixGenerator = (cardValues, size = 6) => {
   //Grid
   gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
 
-  //Cards
-  let cards = document.querySelectorAll(".card-container");
-  cards.forEach((card) => {
-    card.addEventListener("click", () => {
-      //sound will sounded while onklick
-      sound2.play();
-      if (!card.classList.contains("matched")) {
-        card.classList.add("flipped");
-        if (!firstCard) {
-          firstCard = card;
-          firstCardValue = card.getAttribute("data-card-value");
-        } else {
-          movesCounter();
-          secondCard = card;
-          let secondCardValue = card.getAttribute("data-card-value");
-          if (firstCardValue == secondCardValue) {
-            firstCard.classList.add("matched");
-            secondCard.classList.add("matched");
-            firstCard = false;
-            winCount += 1;
-            if (winCount == Math.floor(cardValues.length / 2)){
-              //play sound music
-              sound3.play();
-              result.innerHTML = `<h2>You Winn!</h2>
-              <h4>Moves: ${movesCount}</h4>`;
-              stopGame();
-            }
+  // Cards
+    let cards = document.querySelectorAll(".card-container");
+    cards.forEach((card) => {
+      card.addEventListener("click", () => {
+        sound2.play();
+        if (!card.classList.contains("matched")) {
+          card.classList.add("flipped");
+          if (!firstCard) {
+            firstCard = card;
+            firstCardValue = card.getAttribute("data-card-value");
           } else {
-            let [tempFirst, tempSecond] = [firstCard, secondCard];
-            firstCard = false;
-            secondCard = false;
-            let delay = setTimeout(() => {
-              tempFirst.classList.remove("flipped");
-              tempSecond.classList.remove("flipped");
-            }, 600);
+            movesCounter();
+            secondCard = card;
+            let secondCardValue = card.getAttribute("data-card-value");
+            if (firstCardValue == secondCardValue) {
+              firstCard.classList.add("matched");
+              secondCard.classList.add("matched");
+              firstCard = false;
+              winCount += 1;
+              if (winCount == Math.floor(cardValues.length / 2)) {
+                //play sound music
+                sound3.play();
+                const popup = Swal.fire({
+                  title: "You Win!",
+                  text: "Selamat kamu menang",
+                  icon: "success",
+                  allowEnterKey: true,
+                  isComfirmed: controls.classList.remove("hide"),
+                  isComfirmed: result.classList.add("hide"),
+                  isComfirmed: startButton.classList.add("visible")
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                    sound3.play();
+                  }
+                });
+                result.innerHTML = `${popup}
+                <h4>Peluang: ${movesCount}</h4>`;
+                wrapper.classList.add("hide");
+                stopGame();
+              }
+            } else {
+              let [tempFirst, tempSecond] = [firstCard, secondCard];
+              firstCard = false;
+              secondCard = false;
+              let delay = setTimeout(() => {
+                tempFirst.classList.remove("flipped");
+                tempSecond.classList.remove("flipped");
+              }, 600);
+            }
           }
         }
-      }
+      });
     });
-  });
-};
-
-//Start game
-startButton.addEventListener("click", () => {
-  sound1.play();
-  movesCount = 0;
-  seconds = 0;
-  minutes = 0;
-  //controls amd buttons visibility
-  controls.classList.add("hide");
-  stopButton.classList.remove("hide");
-  startButton.classList.add("hide");
-  //Start timer
-  interval = setInterval(timeGenerator, 1000);
-  //initial moves
-  moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
-  initializer();
-});
+  };
 
 //Stop game
-stopButton.addEventListener("click",
-  (stopGame = () => {
-    sound4.play();
-    controls.classList.remove("hide");
-    stopButton.classList.add("hide");
-    startButton.classList.remove("hide");
-    clearInterval(interval);
-  })
-);
+function gameStop() {
+  sound4.play();
+  controls.classList.remove("hide");
+  wrapper.classList.add("hide");
+  stopButton.classList.add("hide");
+  startButton.classList.remove("hide");
+  clearInterval(interval);
+}
 
 //Initialize values and func calls
 const initializer = () => {
@@ -253,11 +288,11 @@ const initializer = () => {
 };
 
 const body = document.body;
-body.addEventListener('mousemove', function(event){
+body.addEventListener("mousemove", function (event) {
   const lebarContent = window.innerWidth;
   const tinggiContent = window.innerHeight;
   const posX = Math.round((event.clientX / lebarContent) * 255);
   const posY = Math.round((event.clientY / tinggiContent) * 255);
-  
-  body.style.backgroundColor = 'rgb('+posX+','+posY+',80)';
+
+  body.style.backgroundColor = "rgb(" + posX + "," + posY + "," + posX + ")";
 });
